95552475078b3377364489f1c3adecee2a689d5f
Falko Schumann <falko.schumann@muspellheim.de>
2021-01-06 15:13:41 +0100

    [Build] Abhängigkeiten Gradle und JPMS abgeglichen
    - api -> require transitive
    - implementation -> require
    - Außnahme ist automatic module: api -> require (ohne transitive)

--- activity-sampling/src/main/java/module-info.java
+++ activity-sampling/src/main/java/module-info.java
@@ -1,9 +1,7 @@
 module de.muspellheim.activitysampling {
   requires de.muspellheim.activitysampling.backend;
-  requires de.muspellheim.activitysampling.contract;
   requires de.muspellheim.activitysampling.frontend;
-  requires de.muspellheim.messages;
-  requires javafx.graphics;
 
-  exports de.muspellheim.activitysampling;
+  exports de.muspellheim.activitysampling to
+      javafx.graphics;
 }
