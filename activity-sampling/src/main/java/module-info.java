bcbbfdec1fce3365ea28380d8519bebc33cac177
Falko Schumann <falko.schumann@muspellheim.de>
2021-01-06 08:33:25 +0100

    [Build] org.openjfx.javafxplugin ersetzt

--- activity-sampling/src/main/java/module-info.java
+++ activity-sampling/src/main/java/module-info.java
@@ -1,6 +1,6 @@
 module de.muspellheim.activitysampling {
-  requires de.muspellheim.activitysampling.contract;
   requires de.muspellheim.activitysampling.backend;
+  requires de.muspellheim.activitysampling.contract;
   requires de.muspellheim.activitysampling.frontend;
   requires de.muspellheim.messages;
   requires javafx.graphics;
