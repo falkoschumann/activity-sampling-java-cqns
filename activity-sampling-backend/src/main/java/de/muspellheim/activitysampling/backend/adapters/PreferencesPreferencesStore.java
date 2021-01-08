292a20ab0253e848ec1a20562fed3de2942c4272
Falko Schumann <falko.schumann@muspellheim.de>
2021-01-08 19:41:36 +0100

    [Backend] Message Handler vervollständigt

---
+++ activity-sampling-backend/src/main/java/de/muspellheim/activitysampling/backend/adapters/PreferencesPreferencesStore.java
@@ -0,0 +1,42 @@
+/*
+ * Activity Sampling - Backend
+ * Copyright (c) 2021 Falko Schumann <falko.schumann@muspellheim.de>
+ */
+
+package de.muspellheim.activitysampling.backend.adapters;
+
+import de.muspellheim.activitysampling.backend.PreferencesStore;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.Duration;
+import java.util.prefs.Preferences;
+
+public class PreferencesPreferencesStore implements PreferencesStore {
+  private static final String KEY_PREFIX = "de.muspellheim.activitysampling";
+  private static final String KEY_PERIOD_DURATION = KEY_PREFIX + "/periodDuration";
+  private static final String KEY_ACTIVITY_LOG_FILE = KEY_PREFIX + "/activityLogFile";
+
+  private final Preferences preferences = Preferences.userRoot();
+
+  @Override
+  public Duration loadPeriodDuration() {
+    var value = preferences.get(KEY_PERIOD_DURATION, "PT20M");
+    return Duration.parse(value);
+  }
+
+  @Override
+  public void savePeriodDuration(Duration periodDuration) {
+    preferences.put(KEY_PERIOD_DURATION, periodDuration.toString());
+  }
+
+  @Override
+  public Path loadActivityLogFile() {
+    var value = preferences.get(KEY_ACTIVITY_LOG_FILE, "~/activity-log.csv");
+    return Paths.get(value);
+  }
+
+  @Override
+  public void saveActivityLogFile(Path activityLogFile) {
+    preferences.put(KEY_ACTIVITY_LOG_FILE, activityLogFile.toString());
+  }
+}
--- activity-sampling/src/main/java/de/muspellheim/activitysampling/App.java
+++ activity-sampling/src/main/java/de/muspellheim/activitysampling/App.java
@@ -6,11 +6,18 @@
 package de.muspellheim.activitysampling;
 
 import de.muspellheim.activitysampling.backend.EventStore;
+import de.muspellheim.activitysampling.backend.PreferencesStore;
 import de.muspellheim.activitysampling.backend.adapters.CsvEventStore;
 import de.muspellheim.activitysampling.backend.adapters.MemoryEventStore;
+import de.muspellheim.activitysampling.backend.adapters.MemoryPreferencesStore;
+import de.muspellheim.activitysampling.backend.adapters.PreferencesPreferencesStore;
 import de.muspellheim.activitysampling.backend.messagehandlers.ActivityLogQueryHandler;
+import de.muspellheim.activitysampling.backend.messagehandlers.ChangeActivityLogFileCommandHandler;
+import de.muspellheim.activitysampling.backend.messagehandlers.ChangePeriodDurationCommandHandler;
 import de.muspellheim.activitysampling.backend.messagehandlers.LogActivityCommandHandler;
+import de.muspellheim.activitysampling.backend.messagehandlers.PreferencesQueryHandler;
 import de.muspellheim.activitysampling.contract.messages.queries.ActivityLogQuery;
+import de.muspellheim.activitysampling.contract.messages.queries.PreferencesQuery;
 import de.muspellheim.activitysampling.frontend.ActivitySamplingViewController;
 import de.muspellheim.activitysampling.frontend.PreferencesViewController;
 import java.nio.file.Paths;
@@ -19,6 +26,7 @@ import javafx.stage.Stage;
 
 public class App extends Application {
   private EventStore eventStore;
+  private PreferencesStore preferencesStore;
   private boolean useSystemMenuBar = true;
 
   public static void main(String[] args) {
@@ -31,12 +39,15 @@ public class App extends Application {
       System.out.println("Run in demo mode...");
       eventStore = new MemoryEventStore();
       eventStore.setOnRecorded(it -> System.out.println("Logged event: " + it));
+      preferencesStore = new PreferencesPreferencesStore();
     } else {
       var userHome = System.getProperty("user.home");
       var file = Paths.get(userHome, "activity-log.csv");
       System.out.println("Save activity log in: " + file.toAbsolutePath());
       eventStore = new CsvEventStore(file);
+      preferencesStore = new MemoryPreferencesStore();
     }
+
     if (getParameters().getUnnamed().contains("--noSystemMenuBar")) {
       useSystemMenuBar = false;
     }
@@ -45,28 +56,53 @@ public class App extends Application {
   @Override
   public void start(Stage primaryStage) {
     var logActivityCommandHandler = new LogActivityCommandHandler(eventStore);
+    var changePeriodDurationCommandHandler =
+        new ChangePeriodDurationCommandHandler(preferencesStore);
+    var changeActivityLogFileCommandHandler =
+        new ChangeActivityLogFileCommandHandler(preferencesStore);
     var activityLogQueryHandler = new ActivityLogQueryHandler(eventStore);
+    var preferencesQueryHandler = new PreferencesQueryHandler(preferencesStore);
 
     var preferencesStage = new Stage();
     preferencesStage.initOwner(primaryStage);
     var preferencesViewController = PreferencesViewController.create(preferencesStage);
+    preferencesViewController.setOnChangePeriodDurationCommand(
+        cmd -> {
+          changePeriodDurationCommandHandler.handle(cmd);
+          var result = preferencesQueryHandler.handle(new PreferencesQuery());
+          preferencesViewController.display(result);
+          // TODO Handle change period duration
+        });
+    preferencesViewController.setOnChangeActivityLogFileCommand(
+        cmd -> {
+          changeActivityLogFileCommandHandler.handle(cmd);
+          var result = preferencesQueryHandler.handle(new PreferencesQuery());
+          preferencesViewController.display(result);
+          // TODO Handle change activity log file
+        });
+    preferencesViewController.setOnPreferencesQuery(
+        qry -> {
+          var result = preferencesQueryHandler.handle(new PreferencesQuery());
+          preferencesViewController.display(result);
+        });
 
     var activitySamplingViewController =
         ActivitySamplingViewController.create(primaryStage, useSystemMenuBar);
     activitySamplingViewController.setOnOpenPreferences(() -> preferencesStage.show());
     activitySamplingViewController.setOnLogActivityCommand(
-        it -> {
-          logActivityCommandHandler.handle(it);
+        cmd -> {
+          logActivityCommandHandler.handle(cmd);
           var result = activityLogQueryHandler.handle(new ActivityLogQuery());
           activitySamplingViewController.display(result);
         });
     activitySamplingViewController.setOnActivityLogQuery(
-        it -> {
-          var result = activityLogQueryHandler.handle(it);
+        qry -> {
+          var result = activityLogQueryHandler.handle(qry);
           activitySamplingViewController.display(result);
         });
 
     primaryStage.show();
     activitySamplingViewController.run();
+    preferencesViewController.run();
   }
 }
