5d7be9fe1eefd435f538b17877275afab44cb059
Falko Schumann <falko.schumann@muspellheim.de>
2021-01-10 08:18:22 +0100

    [Frontend] About-Dialog ist konfigurierbar

--- activity-sampling-frontend/src/main/java/de/muspellheim/activitysampling/frontend/AboutViewController.java
+++ activity-sampling-frontend/src/main/java/de/muspellheim/activitysampling/frontend/AboutViewController.java
@@ -8,11 +8,15 @@ package de.muspellheim.activitysampling.frontend;
 import javafx.fxml.FXML;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
 public class AboutViewController {
+  @FXML private ImageView icon;
+  @FXML private Label appName;
   @FXML private Label version;
   @FXML private Label copyright;
 
@@ -23,16 +27,27 @@ public class AboutViewController {
     stage.initModality(Modality.APPLICATION_MODAL);
     stage.initStyle(StageStyle.UTILITY);
     stage.setResizable(false);
-    stage.setTitle("About Activity Sampling");
     return factory.getController();
   }
 
-  @FXML
-  private void initialize() {
-    var appVersion = System.getProperty("app.version");
-    version.setText("Version " + appVersion);
+  public void initAppIcon(String url) {
+    icon.setImage(new Image(url, true));
+  }
 
-    var appCopyrightYear = System.getProperty("app.copyrightYear");
-    copyright.setText("Copyright © " + appCopyrightYear + " Falko Schumann");
+  public void initAppName(String name) {
+    getWindow().setTitle("About " + name);
+    appName.setText(name);
+  }
+
+  public void initVersion(String version) {
+    this.version.setText("Version " + version);
+  }
+
+  public void initCopyright(String copyright) {
+    this.copyright.setText("Copyright © " + copyright);
+  }
+
+  private Stage getWindow() {
+    return (Stage) icon.getScene().getWindow();
   }
 }
