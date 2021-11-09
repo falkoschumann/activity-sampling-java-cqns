94b1742c0d51776715e72c028fd802c01c20701b
Falko Schumann <falko.schumann@muspellheim.de>
2021-11-09 19:55:16 +0100

    Aktualisiere TODOs

--- activity-sampling-contract/src/main/java/de/muspellheim/activitysampling/contract/messages/notification/PeriodProgressedNotification.java
+++ activity-sampling-contract/src/main/java/de/muspellheim/activitysampling/contract/messages/notification/PeriodProgressedNotification.java
@@ -8,8 +8,6 @@ package de.muspellheim.activitysampling.contract.messages.notification;
 import java.time.LocalTime;
 import java.util.Objects;
 
-// TODO 2. Event für Period Ended anlegen?
-
 public record PeriodProgressedNotification(LocalTime remaining, double progress) {
   public PeriodProgressedNotification {
     Objects.requireNonNull(remaining, "remaining");
